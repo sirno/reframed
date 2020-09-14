beb96f3c8609d3a8d42aa9e0f9f882dbfcef22f0
Nicolas Ochsner <ochsnern@student.ethz.ch>
2020-09-14 14:33:46 +0200

    Minimal medium is now deterministic. Reformated with black.

--- reframed/cobra/medium.py
+++ reframed/cobra/medium.py
@@ -7,9 +7,23 @@ from warnings import warn
 from math import inf
 
 
-def minimal_medium(model, exchange_reactions=None, direction=-1, min_mass_weight=False, min_growth=1,
-                   max_uptake=100, max_compounds=None, n_solutions=1, validate=True, abstol=1e-6,
-                   warnings=True, milp=True, use_pool=False, pool_gap=None, solver=None):
+def minimal_medium(
+    model,
+    exchange_reactions=None,
+    direction=-1,
+    min_mass_weight=False,
+    min_growth=1,
+    max_uptake=100,
+    max_compounds=None,
+    n_solutions=1,
+    validate=True,
+    abstol=1e-6,
+    warnings=True,
+    milp=True,
+    use_pool=False,
+    pool_gap=None,
+    solver=None,
+):
     """ Minimal medium calculator. Determines the minimum number of medium components for the organism to grow.
 
     Notes:
@@ -57,30 +71,46 @@ def minimal_medium(model, exchange_reactions=None, direction=-1, min_mass_weight
 
     if milp:
         for r_id in exchange_reactions:
-            solver.add_variable('y_' + r_id, 0, 1, vartype=VarType.BINARY, update=False)
+            solver.add_variable("y_" + r_id, 0, 1, vartype=VarType.BINARY, update=False)
     else:
         for r_id in exchange_reactions:
-            solver.add_variable('f_' + r_id, 0, max_uptake, update=False)
+            solver.add_variable("f_" + r_id, 0, max_uptake, update=False)
 
     solver.update()
 
     if milp:
         for r_id in exchange_reactions:
             if direction < 0:
-                solver.add_constraint('c_' + r_id, {r_id: 1, 'y_' + r_id: max_uptake}, '>', 0, update=False)
+                solver.add_constraint(
+                    "c_" + r_id,
+                    {r_id: 1, "y_" + r_id: max_uptake},
+                    ">",
+                    0,
+                    update=False,
+                )
             else:
-                solver.add_constraint('c_' + r_id, {r_id: 1, 'y_' + r_id: -max_uptake}, '<', 0, update=False)
+                solver.add_constraint(
+                    "c_" + r_id,
+                    {r_id: 1, "y_" + r_id: -max_uptake},
+                    "<",
+                    0,
+                    update=False,
+                )
 
         if max_compounds:
-            lhs = {'y_' + r_id: 1 for r_id in exchange_reactions}
-            solver.add_constraint('max_cmpds', lhs, '<', max_compounds, update=False)
+            lhs = {"y_" + r_id: 1 for r_id in exchange_reactions}
+            solver.add_constraint("max_cmpds", lhs, "<", max_compounds, update=False)
 
     else:
         for r_id in exchange_reactions:
             if direction < 0:
-                solver.add_constraint('c_' + r_id, {r_id: 1, 'f_' + r_id: 1}, '>', 0, update=False)
+                solver.add_constraint(
+                    "c_" + r_id, {r_id: 1, "f_" + r_id: 1}, ">", 0, update=False
+                )
             else:
-                solver.add_constraint('c_' + r_id, {r_id: 1, 'f_' + r_id: -1}, '<', 0, update=False)
+                solver.add_constraint(
+                    "c_" + r_id, {r_id: 1, "f_" + r_id: -1}, "<", 0, update=False
+                )
 
     solver.update()
 
@@ -89,7 +119,7 @@ def minimal_medium(model, exchange_reactions=None, direction=-1, min_mass_weight
     if min_mass_weight:
         objective = {}
 
-        multiple_compounds =[]
+        multiple_compounds = []
         no_compounds = []
         no_formula = []
         invalid_formulas = []
@@ -111,11 +141,11 @@ def minimal_medium(model, exchange_reactions=None, direction=-1, min_mass_weight
 
             metabolite = model.metabolites[compounds[0]]
 
-            if 'FORMULA' not in metabolite.metadata:
+            if "FORMULA" not in metabolite.metadata:
                 no_formula.append(metabolite.id)
                 continue
 
-            formula = metabolite.metadata['FORMULA']
+            formula = metabolite.metadata["FORMULA"]
             weight = molecular_weight(formula)
 
             if weight is None:
@@ -123,14 +153,16 @@ def minimal_medium(model, exchange_reactions=None, direction=-1, min_mass_weight
                 continue
 
             if milp:
-                objective['y_' + r_id] = weight
+                objective["y_" + r_id] = weight
             else:
-                objective['f_' + r_id] = weight
+                objective["f_" + r_id] = weight
 
             valid_reactions.append(r_id)
 
         if multiple_compounds:
-            warn_wrapper(f"Reactions ignored (multiple compounds): {multiple_compounds}")
+            warn_wrapper(
+                f"Reactions ignored (multiple compounds): {multiple_compounds}"
+            )
         if no_compounds:
             warn_wrapper(f"Reactions ignored (no compounds): {no_compounds}")
         if multiple_compounds:
@@ -140,46 +172,74 @@ def minimal_medium(model, exchange_reactions=None, direction=-1, min_mass_weight
 
     else:
         if milp:
-            objective = {'y_' + r_id: 1 for r_id in exchange_reactions}
+            objective = {"y_" + r_id: 1 for r_id in exchange_reactions}.update(
+                {model.biomass_reaction: -1e-3}
+            )
         else:
-            objective = {'f_' + r_id: 1 for r_id in exchange_reactions}
+            objective = {"f_" + r_id: 1 for r_id in exchange_reactions}
 
         valid_reactions = exchange_reactions
 
     result, ret_sols = None, None
 
     if direction < 0:
-        constraints = {r_id: (-max_uptake if r_id in valid_reactions else 0, model.reactions[r_id].ub)
-                       for r_id in exchange_reactions}
+        constraints = {
+            r_id: (
+                -max_uptake if r_id in valid_reactions else 0,
+                model.reactions[r_id].ub,
+            )
+            for r_id in exchange_reactions
+        }
     else:
-        constraints = {r_id: (model.reactions[r_id].lb, max_uptake if r_id in valid_reactions else 0)
-                       for r_id in exchange_reactions}
+        constraints = {
+            r_id: (
+                model.reactions[r_id].lb,
+                max_uptake if r_id in valid_reactions else 0,
+            )
+            for r_id in exchange_reactions
+        }
 
     constraints[model.biomass_reaction] = (min_growth, inf)
 
     if n_solutions == 1:
 
-        solution = solver.solve(objective, minimize=True, constraints=constraints, get_values=exchange_reactions)
+        solution = solver.solve(
+            objective,
+            minimize=True,
+            constraints=constraints,
+            get_values=exchange_reactions,
+        )
 
         if solution.status != Status.OPTIMAL:
-            warn_wrapper('No solution found')
+            warn_wrapper("No solution found")
             result, ret_sols = None, solution
         else:
             medium = get_medium(solution, exchange_reactions, direction, abstol)
 
             if validate:
-                validate_solution(model, medium, exchange_reactions, direction, min_growth, max_uptake)
+                validate_solution(
+                    model, medium, exchange_reactions, direction, min_growth, max_uptake
+                )
 
             result, ret_sols = medium, solution
 
     elif use_pool:
-        solutions = solver.solve(objective, minimize=True, constraints=constraints, get_values=exchange_reactions,
-                                 pool_size=n_solutions, pool_gap=pool_gap)
+        solutions = solver.solve(
+            objective,
+            minimize=True,
+            constraints=constraints,
+            get_values=exchange_reactions,
+            pool_size=n_solutions,
+            pool_gap=pool_gap,
+        )
 
         if solutions is None:
             result, ret_sols = [], []
         else:
-            media = [get_medium(solution, exchange_reactions, direction, abstol) for solution in solutions]
+            media = [
+                get_medium(solution, exchange_reactions, direction, abstol)
+                for solution in solutions
+            ]
             result, ret_sols = media, solutions
     else:
         media = []
@@ -188,10 +248,17 @@ def minimal_medium(model, exchange_reactions=None, direction=-1, min_mass_weight
         for i in range(0, n_solutions):
             if i > 0:
                 constr_id = f"iteration_{i}"
-                previous_sol = {'y_' + r_id: 1 for r_id in medium}
-                solver.add_constraint(constr_id, previous_sol, '<', len(previous_sol) - 1)
+                previous_sol = {"y_" + r_id: 1 for r_id in medium}
+                solver.add_constraint(
+                    constr_id, previous_sol, "<", len(previous_sol) - 1
+                )
 
-            solution = solver.solve(objective, minimize=True, constraints=constraints, get_values=exchange_reactions)
+            solution = solver.solve(
+                objective,
+                minimize=True,
+                constraints=constraints,
+                get_values=exchange_reactions,
+            )
 
             if solution.status != Status.OPTIMAL:
                 break
@@ -206,18 +273,33 @@ def minimal_medium(model, exchange_reactions=None, direction=-1, min_mass_weight
 
 
 def get_medium(solution, exchange, direction, abstol):
-    return set(r_id for r_id in exchange
-                 if (direction < 0 and solution.values[r_id] < -abstol
-                     or direction > 0 and solution.values[r_id] > abstol))
+    return set(
+        r_id
+        for r_id in exchange
+        if (
+            direction < 0
+            and solution.values[r_id] < -abstol
+            or direction > 0
+            and solution.values[r_id] > abstol
+        )
+    )
 
 
-def validate_solution(model, medium, exchange_reactions, direction, min_growth, max_uptake):
+def validate_solution(
+    model, medium, exchange_reactions, direction, min_growth, max_uptake
+):
     if direction == -1:
-        constraints = {r_id: (-max_uptake, inf) if r_id in medium else (0, inf) for r_id in exchange_reactions}
+        constraints = {
+            r_id: (-max_uptake, inf) if r_id in medium else (0, inf)
+            for r_id in exchange_reactions
+        }
     else:
-        constraints = {r_id: (-inf, max_uptake) if r_id in medium else (-inf, 0) for r_id in exchange_reactions}
+        constraints = {
+            r_id: (-inf, max_uptake) if r_id in medium else (-inf, 0)
+            for r_id in exchange_reactions
+        }
 
     sol = FBA(model, constraints=constraints)
 
     if sol.fobj < min_growth:
-        warn('Solution appears to be invalid.')
+        warn("Solution appears to be invalid.")
